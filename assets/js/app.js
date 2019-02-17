38b8a4d1a7e12f1bdeeedcab2bea96f9d0f7c670
Bmahdi6 <balmirelmehdi@gmail.com>
2019-02-17 01:50:14 +0100

    Template Integration

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+require('../js/dashboard.js');
+require('../js/vendor.bundle.base');
+// require('../js/vendor.bundle.addons');
+require('../js/off-canvas');
+require('../js/misc');
+require('../css/app.css');
